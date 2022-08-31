41d8122615bb725a626512c7aff1e4edfed21540
Schow94 <Schow94@gmail.com>
2022-08-30 20:54:43 -0700

    Remove .git directory from api & re-add api

---
+++ api/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// Main express app
+const app = express();
+
+const userRoutes = require("./routes/users");
+
+const PORT = process.env.PORT || 8000;
+
+// ------------------------------- MIDDLEWARES ---------------------------
+// Enable CORS for all origins
+app.use(cors());
+// Request logger for NodeJs
+app.use(morgan("tiny"));
+// Parse all requests before handlers. Puts data on req.body
+app.use(bodyParser.json());
+
+// --------------------------------------- Routes ----------------------------------
+app.use("/users", userRoutes);
+
+// ---------------------------- ERROR HANDLING ------------------------
+// Resource not found
+app.use((req, res, next) => {
+	var err = new Error("Not found");
+	err.status = 404;
+	return next(err);
+});
+
+// Send back 500 for other errors
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	return res.json({
+		message: err.message,
+		// Only display err msg when in dev
+		error: app.get("env") === "development" ? err : {},
+	});
+});
+
+// App listens on PORT 8000 or dev
+app.listen(PORT, () => {
+	if (app.get("env") === "development") {
+		console.log(`Listening on PORT: ${PORT}`);
+	} else {
+		console.log("In Production");
+	}
+});
---
+++ api/routes/users.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const SECRET = process.env.SECRET;
+
+// ------------------------ EXPRESS MIDDLEWARES --------------------------
+// Check if user's token is valid
+const isLoggedIn = async (req, res, next) => {
+	try {
+		// Slice to remove 'Bearer' in front of our token
+		const authHeaderValue = req.headers.authorization.slice(7);
+
+		// Verify that token is valid
+		const token = jwt.verify(authHeaderValue, SECRET);
+		// Decode token & grab username
+		const username = jwt.decode(authHeaderValue).username;
+		// Check that user is a valid row in db
+		const user = await db.query("SELECT * FROM users WHERE username=$1", [
+			username,
+		]);
+
+		req.username = username;
+		req.user_id = user.rows[0].id;
+
+		return next();
+	} catch (e) {
+		// Invalid token
+		return res.status(401).json({ message: "Unauthorized" });
+	}
+};
+
+// -------------------------- USER ROUTES ---------------------------
+/*
+	- Get list of all users
+*/
+router.get("/", isLoggedIn, async (req, res, next) => {
+	try {
+		// Query db for all users. Don't return hashed pw to client
+		const result = await db.query("SELECT id, username, email FROM users");
+
+		// Send list of users in db back to user
+		return res.json(result.rows);
+	} catch (e) {
+		return next(e);
+	}
+});
+
+/*
+	- Signup Route
+*/
+router.post("/signup", async (req, res, next) => {
+	try {
+		// bcrypt.hash(unhashedPw, saltRounds)
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+		// Add new user to db
+		const result = await db.query(
+			"INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+			[req.body.username, req.body.email, hashedPassword]
+		);
+
+		// Sign token w/ username
+		const token = jwt.sign(
+			{
+				username: req.body.username,
+			},
+			SECRET,
+			{ expiresIn: 60 * 60 }
+		);
+
+		// Send token back to user
+		return res.json({ token });
+	} catch (e) {
+		console.log(e);
+		return next(e);
+	}
+});
+
+/*
+	- Login Route
+*/
+router.post("/login", async (req, res, next) => {
+	try {
+		// Check if user in db
+		const foundUser = await db.query(
+			"SELECT * FROM users WHERE username=$1 LIMIT 1",
+			[req.body.username]
+		);
+
+		// user not found in db
+		if (foundUser.rows.length === 0) {
+			return res.json({ message: "Invalid Username" });
+		}
+
+		// compare hashedPw to unhashedPw
+		const verifiedPw = await bcrypt.compare(
+			req.body.password,
+			foundUser.rows[0].password
+		);
+
+		// invalid pw
+		if (verifiedPw === false) {
+			return res.json({ message: "Invalid Password" });
+		}
+
+		// Credentials were valid
+		const token = jwt.sign({ username: foundUser.rows[0].username }, SECRET, {
+			expiresIn: 60 * 60,
+		});
+
+		// Send token back to user
+		return res.json({ token });
+	} catch (e) {
+		return next(e);
+	}
+});
+
+module.exports = router;
+
+/*
+	============== Create Database on Heroku/local ==============
+		CREATE DATABASE woebot_takehome;
+
+	============== Create table ==============
+		CREATE TABLE users (
+			id SERIAL PRIMARY KEY,
+			username TEXT UNIQUE,
+			password TEXT,
+			email TEXT UNIQUE
+		);
+
+	============== Insert into table ==============
+		INSERT INTO users (
+			username,
+			email,
+			password
+		) VALUES (
+			'admin',
+			'admin@admin.com',
+			'password'
+		);
+
+*/
